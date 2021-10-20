cfd665142cf37b64aac35209ca4282049e704d20
Teilor Souza Barcelos <tsb-@live.com>
2021-10-20 13:23:48 -0300

    finished and runing att https://impulsenode.herokuapp.com/

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express"
 import { MessageController } from "./controllers/messageController"
 import { UserController } from "./controllers/userController"
 import { onlyAuth } from "./middleware/onlyAuth"
 
 const router = Router()
+
+router.get("/", (request: Request, response: Response) => {
+  const message = {
+    message: "API created on NLW7 (HEAT) event by Rocketseat Author: Teilor Souza Barcelos",
+    supportedRoutes: `/authenticate, /userProfile, /messageCreate, /getLast3Messages`
+  }
+  return response.json(message)
+})
 
 router.post("/authenticate", new UserController().handleAuth)
 router.get("/userProfile", onlyAuth, new UserController().handleGetUserProfile)
